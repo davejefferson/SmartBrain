6cb5dc3791a7fbb645e5ab13a4bec4c8d4445ab9
davejefferson <36189939+davejefferson@users.noreply.github.com>
2018-06-01 20:59:20 +0100

    add FaceRecognition component and install and hook up Clarifai face regcontion api

--- smart-brain/src/App.js
+++ smart-brain/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from "clarifai";
 import Navigation from "./Components/Navigation/Navigation";
 import Logo from "./Components/Logo/Logo";
 import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
 import Rank from "./Components/Rank/Rank";
+import FaceRecognition from "./Components/FaceRecognition/FaceRecognition";
 import './App.css';
 
 
+const app = new Clarifai.App({
+  apiKey: 'eeb49ba2de0c4ff7807a01714bdd341f'
+});
 
 const particlesOptions = {
    particles: {
@@ -22,6 +27,34 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: ""
+    }
+  }
+
+  onInputChange =(event) => {
+    this.setState({input: event.target.value});
+  }
+
+onButtonSubmit = () => {
+  this.setState({imageUrl: this.state.input})
+  app.models.predict(
+    Clarifai.FACE_DETECT_MODEL,
+    this.state.input)
+    .then(
+    function (response) {
+      // do something with response
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+    },
+    function (err) {
+      // there was an error
+    }
+  );
+}
+
   render() {
     return (
       <div className="App">
@@ -31,9 +64,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm 
+        onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
 
-        {/* <FaceRecognition /> */}
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
