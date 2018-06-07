22c3db435a813b3b6ff47c0d35a9683167537cd8
davejefferson <36189939+davejefferson@users.noreply.github.com>
2018-06-07 16:32:35 +0100

    test bcyrpt

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -1,5 +1,6 @@
 const express= require("express");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,19 +11,24 @@ const database = {
             id: "123",
             name: "John",
             email: "john@gmail.com",
-            password: "cookies",
             entries: 0,
             joined: new Date()
         },
         {
             id: "124",
             name: "Sally",
-            email: "sally@gmail.com",
             password: "bananas",
             entries: 0,
             joined: new Date()
         }
 
+    ],
+    login: [
+        {
+            id:"987",
+            has:"",
+            email:"john@gmail.com"
+        }
     ]
 }
 
@@ -31,11 +37,19 @@ app.get("/", (req, res)=>{
 });
 
 app.post("/signin", (req, res)=> {
+    // Load hash from your password DB.
+    bcrypt.compare("apples", "$2a$10$c0dceuuaRwN42qZxmxDWTObZcp9dBE5rf7t7d8owv4qJoDef4yuOy", function (err, res) {
+        console.log("first guess", res)
+    });
+    bcrypt.compare("veggies", "$2a$10$c0dceuuaRwN42qZxmxDWTObZcp9dBE5rf7t7d8owv4qJoDef4yuOy", function (err, res) {
+        // res = false
+        console.log("second guess", res)
+    });
     if(req.body.email === database.users[0].email &&
          req.body.password === database.users[0].password){
             res.json("success");
          } else {
-             res.statusend(400).json("error logging in");
+             res.status(400).json("error logging in");
          }
 
     
@@ -43,6 +57,9 @@ app.post("/signin", (req, res)=> {
 
 app.post("/register", (req, res)=>{
     const { email, name, password } = req.body;
+    bcrypt.hash(password, null, null, function (err, hash) {
+        console.log(hash);
+    });
     database.users.push({
         id: "125",
         name: name,
@@ -82,6 +99,10 @@ app.put("/image", (req, res) => {
         res.status(400).json("not found");
     }
 })
+
+
+
+
 
 app.listen(3000, ()=> {
     console.log("app is running on port 3000")
