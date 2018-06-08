75d3f5e67c358eaea079012acc279fd49f080d76
davejefferson <36189939+davejefferson@users.noreply.github.com>
2018-06-08 14:06:45 +0100

    install cors npm start connecting front and backend

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -1,9 +1,11 @@
 const express= require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
     users: [
@@ -104,8 +106,8 @@ app.put("/image", (req, res) => {
 
 
 
-app.listen(3000, ()=> {
-    console.log("app is running on port 3000")
+app.listen(3001, ()=> {
+    console.log("app is running on port 3001")
 })
 
 
--- smart-brain/src/App.js
+++ smart-brain/src/App.js
@@ -40,6 +40,11 @@ class App extends Component {
     }
   }
 
+  componentDidMount() {
+    fetch("http://localhost:3001/")
+    .then(response => response.json())
+    .then(console.log)
+  }
 
   calculateFaceLocation = (data) => {
     const clarifiaFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -54,6 +59,7 @@ class App extends Component {
     }
   }
 
+ 
   displayFaceBox = (box) => {
     console.log(box)
     this.setState({box: box})
